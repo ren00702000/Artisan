4c97a0aaa97360103a96d6edd851208260f559ed
ren00702000 <kharen.silna@gmail.com>
2026-02-24 13:26:45 -0300

    Base

---
+++ js/products.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    id: 1,
+    title: "Camiseta Essential",
+    price: "R$ 129,90",
+    image: "https://via.placeholder.com/300x400"
+  },
+  {
+    id: 2,
+    title: "Moletom Minimal",
+    price: "R$ 289,90",
+    image: "https://via.placeholder.com/300x400"
+  }
+];
